c81a11847c907052660b55fd289ebc694631f19a
kthss01 <kthss01@naver.com>
2018-03-05 19:25:09 +0900

    록맨 풀이(완전 풀이는 아님) 및 디버그 설명

--- Day-33/MainGame14.cpp
+++ Day-33/MainGame14.cpp
@@ -15,17 +15,191 @@ HRESULT MainGame14::Init()
 {
 	GameNode::Init();
 
+	bg = new Image;
+	bg->Init("images/메가맨배경.bmp", WINSIZEX, WINSIZEY);
+
+	// 이런식으로 설정시 문제가 될 수 있음
+	// 화면 창 크기가 변화가 되었을 때 문제가 발생함
+	// 문제가 없도록 하려면 기준점으로 짜야하긴함 
+	box[0] = { 0, 572, WINSIZEX, WINSIZEY };
+	box[1] = { 704, 486, 1025, 510 };
+	box[2] = { 1026, 223, WINSIZEX, 223 };
+	box[3] = { 960, 190, 1026, 260 };
+
+	for (int i = 0; i < 11; i++) {
+		character[0][STATUS_ONLAND][i] = new Image;
+		sprintf_s(str, "images/왼쪽걷기%d.bmp", i + 1);
+		character[0][STATUS_ONLAND][i]->Init(str, SIZEX, SIZEY,
+			true, RGB(255, 0, 255));
+		character[1][STATUS_ONLAND][i] = new Image;
+		sprintf_s(str, "images/오른쪽걷기%d.bmp", i + 1);
+		character[1][STATUS_ONLAND][i]->Init(str, SIZEX, SIZEY,
+			true, RGB(255, 0, 255));
+	}
+
+	for (int i = 0; i < 6; i++) {
+		character[0][STATUS_NORMAL][i] = new Image;
+		sprintf_s(str, "images/왼쪽평시%d.bmp", i + 1);
+		character[0][STATUS_NORMAL][i]->Init(str, SIZEX, SIZEY,
+			true, RGB(255, 0, 255));
+		character[1][STATUS_NORMAL][i] = new Image;
+		sprintf_s(str, "images/오른쪽평시%d.bmp", i + 1);
+		character[1][STATUS_NORMAL][i]->Init(str, SIZEX, SIZEY,
+			true, RGB(255, 0, 255));
+	}
+
+	for (int i = 0; i < 5; i++) {
+		character[0][STATUS_TOUP][i] = new Image;
+		sprintf_s(str, "images/왼쪽점프%d.bmp", i + 1);
+		character[0][STATUS_TOUP][i]->Init(str, SIZEX, SIZEY,
+			true, RGB(255, 0, 255));
+		character[1][STATUS_TOUP][i] = new Image;
+		sprintf_s(str, "images/오른쪽점프%d.bmp", i + 1);
+		character[1][STATUS_TOUP][i]->Init(str, SIZEX, SIZEY,
+			true, RGB(255, 0, 255));
+
+		character[0][STATUS_TOLOW][i] = new Image;
+		sprintf_s(str, "images/왼쪽점프%d.bmp", i + 6);
+		character[0][STATUS_TOLOW][i]->Init(str, SIZEX, SIZEY,
+			true, RGB(255, 0, 255));
+		character[1][STATUS_TOLOW][i] = new Image;
+		sprintf_s(str, "images/오른쪽점프%d.bmp", i + 6);
+		character[1][STATUS_TOLOW][i]->Init(str, SIZEX, SIZEY,
+			true, RGB(255, 0, 255));
+	}
+
+	// 점프 11 12 는 착지 모션
+	character[0][STATUS_LANDING][0] = new Image;
+	character[0][STATUS_LANDING][0]->Init("images/왼쪽점프11.bmp",
+		SIZEX, SIZEY, true, RGB(255, 0, 255));
+	character[0][STATUS_LANDING][1] = new Image;
+	character[0][STATUS_LANDING][1]->Init("images/왼쪽점프12.bmp",
+		SIZEX, SIZEY, true, RGB(255, 0, 255));
+
+	character[1][STATUS_LANDING][0] = new Image;
+	character[1][STATUS_LANDING][0]->Init("images/오른쪽점프11.bmp",
+		SIZEX, SIZEY, true, RGB(255, 0, 255));
+	character[1][STATUS_LANDING][1] = new Image;
+	character[1][STATUS_LANDING][1]->Init("images/오른쪽점프12.bmp",
+		SIZEX, SIZEY, true, RGB(255, 0, 255));
+
+	// 1 벽에 매달리는 모션 2,3 은 올라가는 모션
+	character[0][STATUS_WALL][0] = new Image;
+	character[0][STATUS_WALL][0]->Init("images/왼쪽벽잡기1.bmp",
+		SIZEX, SIZEY, true, RGB(255, 0, 255));
+
+	character[0][STATUS_CLIMB][0] = new Image;
+	character[0][STATUS_CLIMB][0]->Init("images/왼쪽벽잡기2.bmp",
+		SIZEX, SIZEY, true, RGB(255, 0, 255));
+	character[0][STATUS_CLIMB][1] = new Image;
+	character[0][STATUS_CLIMB][1]->Init("images/왼쪽벽잡기3.bmp",
+		SIZEX, SIZEY, true, RGB(255, 0, 255));
+
+	character[1][STATUS_WALL][0] = new Image;
+	character[1][STATUS_WALL][0]->Init("images/오른쪽벽잡기1.bmp",
+		SIZEX, SIZEY, true, RGB(255, 0, 255));
+
+	character[1][STATUS_CLIMB][0] = new Image;
+	character[1][STATUS_CLIMB][0]->Init("images/오른쪽벽잡기2.bmp",
+		SIZEX, SIZEY, true, RGB(255, 0, 255));
+	character[1][STATUS_CLIMB][1] = new Image;
+	character[1][STATUS_CLIMB][1]->Init("images/오른쪽벽잡기3.bmp",
+		SIZEX, SIZEY, true, RGB(255, 0, 255));
+
+	status = STATUS_NORMAL;
+
 	return S_OK;
 }
 
 void MainGame14::Release()
 {
 	GameNode::Release();
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 6; j++) {
+			for (int k = 0; k < 11; k++) {
+				// 존재하면 지우겠다는거 들어가 있는게 다 다르니
+				if (character[i][j][k])
+					SAFE_DELETE(character[i][j][k]);
+			}
+		}
+	}
+	SAFE_DELETE(bg);
 }
 
 void MainGame14::Update()
 {
 	GameNode::Update();
+
+	// 현재 애니메이션 클립당 어떠한 동작을 취할지 진행 할 꺼
+	// Move() Jump()에선 실제 어떻게 바뀔지 적용할 꺼
+	switch (status)
+	{
+	// 가만히 서있는 경우
+	case STATUS_NORMAL:
+		// 점프하거나 걸을 수 있음
+		// 점프 함수 실행해서 되었는지 판단
+		Jump();
+		moveFrame += 0.1f;
+		if (moveFrame >= 6)
+			moveFrame = 0;
+
+		// 왼쪽 키 또는 오른쪽 키 누른 경우
+		// 이동하는 녀석으로 상태 변경
+		if (INPUT->GetKey(VK_LEFT) || INPUT->GetKey(VK_RIGHT)) {
+			status = STATUS_ONLAND;
+			moveFrame = 0;
+		}
+		// 아래쪽 키를 눌렀을 때
+		// 밑바닥이 아닌 경우 떨어져야 하므로 떨어지는 상태로 변경
+		if (INPUT->GetKeyDown(VK_DOWN) && boxNum != 0) {
+			status = STATUS_TOLOW;
+			moveFrame = 0;
+			// 중력값
+			vy = 0;
+			y += 30;
+		}
+		break;
+
+	// 움직이고 있을 때
+	case STATUS_ONLAND:
+		Move();
+		Jump();
+		// 박스 밟고 있다 떨어질 때 
+		// 내 중심 좌표에서 왼쪽 오른쪽 좌표보다 크거나 작아지면
+		if (box[boxNum].left - SIZEX / 2 > x ||
+			box[boxNum].right + SIZEX / 2 < x) {
+			status = STATUS_TOLOW;
+			moveFrame = 0;
+			vy = 0;
+		}
+		if (INPUT->GetKeyUp(VK_LEFT) || INPUT->GetKeyUp(VK_RIGHT)) {
+			status = STATUS_NORMAL;
+			moveFrame = 0;
+		}
+		// 아래쪽 키를 눌렀을 때
+		// 밑바닥이 아닌 경우 떨어져야 하므로 떨어지는 상태로 변경
+		if (INPUT->GetKeyDown(VK_DOWN) && boxNum != 0) {
+			status = STATUS_TOLOW;
+			moveFrame = 0;
+			// 중력값
+			vy = 0;
+			y += 30;
+		}
+		break;
+	case STATUS_TOUP:
+		break;
+	case STATUS_TOLOW:
+		break;
+	case STATUS_LANDING:
+		break;
+	case STATUS_WALL:
+		break;
+	case STATUS_CLIMB:
+		break;
+	default:
+		break;
+	}
 }
 
 void MainGame14::Render(HDC hdc)
@@ -33,7 +207,45 @@ void MainGame14::Render(HDC hdc)
 	HDC memDC = GetBackBuffer()->GetMemDC();
 	PatBlt(memDC, 0, 0, WINSIZEX, WINSIZEY, WHITENESS);
 	//=================================================
-
+	character[vector][status][(int)moveFrame]->
+		Render(memDC, x - SIZEX / 2, y - SIZEY / 2);
 	//=================================================
 	this->GetBackBuffer()->Render(hdc);
+}
+
+void MainGame14::Move()
+{
+	if (INPUT->GetKey(VK_LEFT)) {
+		// 화면 밖으로 나가지 않게끔
+		if (x > SIZEX / 2) {
+			x -= SPEED;
+		}
+		// 방향 0
+		vector = 0;
+		// 뛰고있는 상태인 경우
+		if (status == STATUS_ONLAND) {
+			moveFrame += 0.25f;
+			if (moveFrame >= 11)
+				moveFrame = 0;
+		}
+	}
+
+	if (INPUT->GetKey(VK_RIGHT)) {
+		// 화면 밖으로 나가지 않게끔
+		if (x < WINSIZEX - SIZEX / 2) {
+			x += SPEED;
+		}
+		// 방향 1
+		vector = 1;
+		// 뛰고있는 상태인 경우
+		if (status == STATUS_ONLAND) {
+			moveFrame += 0.25f;
+			if (moveFrame >= 11)
+				moveFrame = 0;
+		}
+	}
+}
+
+void MainGame14::Jump()
+{
 }
--- Day-33/MainGame14.h
+++ Day-33/MainGame14.h
@@ -2,9 +2,54 @@
 
 #include "GameNode.h"
 
+// 과제 풀이
+
+#define SIZEX 117
+#define SIZEY 138
+#define SPEED 7
+#define GRAVITY 0.5f	// 점프했을 때 중력값 받을 정도
+#define JUMP -15		// 얼마 정도 점프가 되었는지
+
+enum STATUS {
+	STATUS_NORMAL,
+	STATUS_ONLAND,
+	STATUS_TOUP,
+	STATUS_TOLOW,		// 좌표값이 밑으로 내려가는거 떨어지는 애니메이션 출력
+	STATUS_LANDING,		// 말그대로 떨어지는거 일정 애니메이션만 돌릴꺼
+	STATUS_WALL,
+	STATUS_CLIMB		// 벽을 잡고 있다가 올라가는 상태
+};
+
 class MainGame14 : public GameNode
 {
 private:
+	STATUS status;
+	
+	// moveFrame 움직이는 애니메이션 
+	// 이건 frame 계산할 때 쓸거 중첩으로 더해져서 쓰게 할 꺼
+	// 좀더 frame 정확하게 되게 하기 위해서 계싼을 위해 사용할 꺼 
+	// (결국엔 int 형 넣긴함)
+	float x = 100, y = 572 - SIZEY / 2, moveFrame = 0;
+	
+	float vy;
+
+	// 0 , 1 내가 발판으로 쓰고있을 녀석, 
+	// 2 젤 위에 있는 녀석, 
+	// 3이 충돌햇을 때 벽 잡는 모션 있는 녀석
+	RECT box[4]; 
+	RECT temp; // 충돌할 때마다 쓸 녀석
+	
+	// vector 애니메이션 돌릴 때 쓸 녀석
+	// 애니메이션 3중 벡터 쓸 거
+	int vector = 1, boxNum = 0;
+	
+	// 캐릭터 [] 왼쪽 오른쪽이냐
+	// 캐릭터 [][] 애니메이션 동작 6가지 뛰는거 걷는거 점프하는거 같은
+	// 캐릭터 [][][] 해당 애니메이션의 프레임
+	Image* bg, *character[2][7][11];
+	
+
+	char str[128]; // 파일 부를때 쓸거 sprintf_s 이용할 때
 
 public:
 	MainGame14();
@@ -14,5 +59,60 @@ public:
 	void Release() override;
 	void Update() override;
 	void Render(HDC hdc) override;
+
+	void Move(); // 이동할 때 애니메이션
+	void Jump(); // 점프할 때
 };
 
+/*
+	atlas xml site
+	이거 이용해서 
+*/
+
+/*
+	디버깅 하는 방법
+		빌드 실패인 경우
+			오타인 경우가 많음
+		메모리 초기화 안해 준 경우
+			예외가 발생함 엑세스 위반 뜸 
+			찾기 쉬운 상태에서 디버그 클릭 후 호출 스택 볼 수 있음
+			호출 스택 어떠한 순서대로 함수 호출했는지 볼 수 있는거
+			역순으로 보여줌
+			추가적으로 어떤 문제인지 보려면 자동을 보면됨
+		외부기호 참조 문제
+			두가지 있음
+				함수 정의 안햇을 때 이렇게 뜸
+				함수 호출 할 수는 있는데 내용 사용할 수 없을 때 뜸
+				자세히 읽어보면 찾을 수는 잇음
+				
+			ex) #pragma comment(lib, "msimg32.lib") 주석 처리 한 겨우
+			AlphaBlend 함수에서 문제가 발생하고 구글에 검색해서 해결
+
+		잘 동작하는거 같은데 실행 중에 에러 뜨는 경우
+		무한반복 하는 경우가 많음 (초기화 같은거를)
+
+		천개 정도 반복문 돌 때
+		특정 번째에 대해서 확인하고 싶을 때
+		반복문 안에 찍은 후
+		디버그 안에 조사식이라고 있음 
+		조사식은 임의의 값 조사하는 것
+		조사식에 i를 쓰고
+			i 값을 수정하고 F10 누르면 디버그 값도 수정 됨 
+			i 5로하면 0~4까지 다 실행하고 나서의 값임
+
+		중단점
+			특정한 녀석에서 값을 확인하기 위한 방법
+			왼쪽 끝에 누르거나 그 라인에서 F9 누름 됨
+			F5누르면 그 라인에서 멈춤
+			F10 누르면 한 칸 내려가고 F11 누르면 함수가 있을시 함수로 들어감
+
+		API에서는 F5누르는걸 추천함 콘솔에서처럼 Ctrl + F5 누르지말고
+		콘솔에서 F5누르면 창이 안켜져서 Ctrl + F5 누르라고 알려주신 거
+
+		비주얼 문제도 비주얼도 디버그 할 수 있음
+*/
+
+/*
+	과제
+	섯다 이미지 넣어서 만들어 오기
+*/
