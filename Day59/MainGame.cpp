a0056fb5dffa6b47095836e2b69dc25be4c3d09d
kthss01 <kthss01@naver.com>
2018-04-10 21:14:05 +0900

    맵툴 Save Load 구현 및 내일 쓸 맵 구현

---
+++ Day59/MainGame.cpp
@@ -0,0 +1,221 @@
+#include "stdafx.h"
+#include "MainGame.h"
+
+#include "TestScene.h"
+
+MainGame::MainGame()
+{
+	SUBWIN->Init();
+}
+
+
+MainGame::~MainGame()
+{
+}
+
+HRESULT MainGame::Init()
+{
+	GameNode::Init();
+	isDebug = false;
+
+	SUBWIN->SetMainGame(this);
+
+	TestScene * test = new TestScene;
+	SCENE->AddScene("Test", test);
+
+	SOUND->Init();
+	SOUND->AddSound("Test", "sounds/영전3.wav", true, true);
+
+	SUBWIN->SetScene(test);
+
+	SCENE->ChangeScene("Test");
+
+	IMAGE->AddImage("tileMap", "images/tilemap.bmp", 0, 0,
+			256, 640, 8, 20, false, RGB(0, 0, 0));
+
+	imageFrame = { 7, 2 };
+
+	MapToolSetup();
+
+	_currentCTRL = CTRL_TERRAINDRAW;
+
+	return S_OK;
+}
+
+void MainGame::Release()
+{
+	GameNode::Release();
+}
+
+void MainGame::Update()
+{
+	GameNode::Update();
+	SUBWIN->Update();
+	SCENE->Update();
+
+	// 이거 문제 있음 해결방안 찾아보자
+	// 마우스 움직이는 윈도우 위치에 따라 active 값 바꾸게했음
+	if (SUBWIN->GetIsActive() == false) {
+		if (INPUT->GetKey(VK_LBUTTON)) {
+			SetMap();
+		}
+	}
+
+	//====================== Debug =====================//
+	if (INPUT->GetKeyDown(VK_F11)) {
+		isDebug = !isDebug;
+	}
+	//==================================================//
+}
+
+void MainGame::Render()
+{
+	PatBlt(GetMemDC(), 0, 0, WINSIZEX, WINSIZEY, WHITENESS);
+	//=================================================
+	{
+		SCENE->Render();
+		SUBWIN->Render();
+
+		for (int i = 0; i < TILEX * TILEY; i++) {
+			// 지형
+			IMAGE->FrameRender("tileMap", GetMemDC(),
+				_tiles[i].rc.left, _tiles[i].rc.top,
+				_tiles[i].terrainFrameX, _tiles[i].terrainFrameY);
+
+			// 오브젝트
+			if (_tiles[i].obj == OBJ_NONE) continue;
+			IMAGE->FrameRender("tileMap", GetMemDC(),
+				_tiles[i].rc.left, _tiles[i].rc.top,
+				_tiles[i].objFrameX, _tiles[i].objFrameY);
+		}
+	}
+	//==================   Debug   ====================
+	if (isDebug)
+	{
+		char str[128];
+		sprintf_s(str, "%d %d", g_ptMouse.x, g_ptMouse.y);
+		TextOut(GetMemDC(), 0, 80, str, strlen(str));
+
+		sprintf_s(str, "%d %d", SUBWIN->GetMousePos().x, SUBWIN->GetMousePos().y);
+		TextOut(GetMemDC(), 0, 100, str, strlen(str));
+	}
+	//=================================================
+	FRAME->Render(GetMemDC());
+	this->SetBackBuffer()->Render(GetHDC());
+}
+
+void MainGame::MapToolSetup()
+{
+	for (int i = 0; i < TILEY; i++) {
+		for (int j = 0; j < TILEX; j++) {
+			SetRect(&_tiles[i * TILEX + j].rc,
+				j * TILESIZE,				// left
+				i * TILESIZE,				// top
+				j * TILESIZE + TILESIZE,	// right
+				i * TILESIZE + TILESIZE);	// bottom
+				
+		}
+	}
+
+	// 처음 깔려 있는 타일
+	for (int i = 0; i < TILEX * TILEY; i++) {
+		_tiles[i].terrainFrameX = 7;
+		_tiles[i].terrainFrameY = 2;
+
+		_tiles[i].objFrameX = 0;
+		_tiles[i].objFrameY = 0;
+
+		_tiles[i].terrain = 
+			TerrainSelect(_tiles[i].terrainFrameX, _tiles[i].terrainFrameY);
+
+		_tiles[i].obj = OBJ_NONE;
+	}
+}
+
+void MainGame::SetMap()
+{
+	imageFrame = SUBWIN->GetFramePoint();
+	
+	// static이라 하나만 있어서 클래스로 가져 올 수 있음
+	// 싱글턴 안쓰고도
+	_currentCTRL = Window::GetCTRL();
+
+	for (int i = 0; i < TILEX*TILEY; i++) {
+		if (PtInRect(&_tiles[i].rc, g_ptMouse)) {
+			if (_currentCTRL == CTRL_TERRAINDRAW) {
+				_tiles[i].terrainFrameX = imageFrame.x;
+				_tiles[i].terrainFrameY = imageFrame.y;
+
+				_tiles[i].terrain = TerrainSelect(imageFrame.x, imageFrame.y);
+				break;
+			}
+			if (_currentCTRL == CTRL_OBJECTDRAW) {
+				_tiles[i].objFrameX = imageFrame.x;
+				_tiles[i].objFrameY = imageFrame.y;
+
+				_tiles[i].obj = ObjSelect(imageFrame.x, imageFrame.y);
+
+				// test
+				_tiles[i].objFrameX = imageFrame.y;
+				_tiles[i].objFrameY = imageFrame.x;
+
+				_tiles[i].obj = ObjSelect(imageFrame.y, imageFrame.x);
+				break;
+			}
+
+			if (_currentCTRL == CTRL_ERASER) {
+				_tiles[i].terrainFrameX = 7;
+				_tiles[i].terrainFrameY = 2;
+
+				_tiles[i].terrain = TerrainSelect(7, 2);
+
+				// object도 지워야한다면 같이 지워주면 됨 자유롭게하기
+				break;
+			}
+		}
+	}
+}
+
+TERRAIN MainGame::TerrainSelect(int frameX, int frameY)
+{
+	// 시멘트 타일
+	if (frameX == 7 && frameY == 1) return TR_CEMENT;
+	// 흙 타일
+	if (frameX == 7 && frameY == 2) return TR_GROUND;
+	// 풀 타일
+	if (frameX == 7 && frameY == 3) return TR_GRASS;
+	// 물 타일
+	if (frameX == 7 && frameY == 4) return TR_WATER;
+}
+
+OBJECT MainGame::ObjSelect(int frameX, int frameY)
+{
+	return OBJECT();
+}
+
+void MainGame::Save()
+{
+	for (int i = 0; i < TILEX*TILEY; i++) {
+		sprintf_s(str, "tile %d", i);
+		INIDATA->AddData(str, "obj", to_string(_tiles[i].obj).c_str());
+		INIDATA->AddData(str, "objFrameX", to_string(_tiles[i].objFrameX).c_str());
+		INIDATA->AddData(str, "objFrameY", to_string(_tiles[i].objFrameY).c_str());
+		INIDATA->AddData(str, "terrain", to_string(_tiles[i].terrain).c_str());
+		INIDATA->AddData(str, "terrainFrameX", to_string(_tiles[i].terrainFrameX).c_str());
+		INIDATA->AddData(str, "terrainFrameY", to_string(_tiles[i].terrainFrameY).c_str());
+	}
+	INIDATA->SaveData("TilesInfo");
+}
+
+void MainGame::Load()
+{
+	for (int i = 0; i < TILEX*TILEY; i++) {
+		sprintf_s(str, "tile %d", i);
+		_tiles[i].obj = (OBJECT)INIDATA->LoadDataInteger("TilesInfo", str, "obj");
+		_tiles[i].objFrameX = INIDATA->LoadDataInteger("TilesInfo", str, "objFrameX");
+		_tiles[i].objFrameY = INIDATA->LoadDataInteger("TilesInfo", str, "objFrameY");
+		_tiles[i].terrain = (TERRAIN)INIDATA->LoadDataInteger("TilesInfo", str, "terrain");
+		_tiles[i].terrainFrameX = INIDATA->LoadDataInteger("TilesInfo", str, "terrainFrameX");
+		_tiles[i].terrainFrameY = INIDATA->LoadDataInteger("TilesInfo", str, "terrainFrameY");
+	}
+}
---
+++ Day59/Window.cpp
@@ -0,0 +1,224 @@
+#include "stdafx.h"
+#include "Window.h"
+#include "GameNode.h"
+
+#include "MainGame.h"
+
+POINT Window::ptMouse = POINT{ 0,0 };
+CTRL Window::_currentCTRL = CTRL_TERRAINDRAW;
+
+// define cpp에 하면 여기만 사용 가능하게 됨 include 할때
+
+#define SUBWINSIZEX 400
+#define SUBWINSIZEY 600
+
+Window::Window()
+{
+	// 로그 윈도우 생성
+	int x, y, cx, cy;
+	WNDCLASS wc;
+	RECT rc;
+
+	wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wc.lpfnWndProc = (WNDPROC)Window::WndLogProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = GetModuleHandle(NULL);
+	wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = "sub";
+
+	RegisterClass(&wc);
+
+	// 부모 윈도우 오른쪽에 위치하게
+	RECT rcWin;
+	GetWindowRect(g_hWnd, &rcWin);
+
+	cx = 250;
+	cy = 600;
+	x = rcWin.right;
+	y = rcWin.top;
+
+	HWND		hParenthWnd = NULL;
+	HINSTANCE	hInst = NULL;
+
+	hWnd = CreateWindow(
+		"sub",
+		"sub",
+		WS_POPUP | WS_CAPTION | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
+		x, y, cx, cy, hParenthWnd, NULL,
+		hInst, NULL);
+
+	ShowWindow(hWnd, SW_SHOW);
+
+	Init();
+
+	m_backBuffer = new Image();
+	m_backBuffer->Init(SUBWINSIZEX, SUBWINSIZEY);
+}
+
+
+Window::~Window()
+{
+	SAFE_DELETE(m_backBuffer);
+}
+
+void Window::Init()
+{
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 20; j++) {
+			rc[i][j] = RectMake(i * 32, j * 32, 32, 32);
+		}
+	}
+	clickFrame = { 0,0 };
+
+	// 버튼도 조그만한 윈도우라고 생각하면 됨
+	// MFC 문법임 색깔을 바꾸거나 이러긴 힘듬
+	// 클릭시 HMENU 메시지 발생
+	_btnTerrainDraw = CreateWindow("button", "Terrain",
+		// 자식으로 생성하면 안쪽에 만들어짐
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+		// 클릭했을때 어떤 값을 반환할지 hMenu
+		280, 50, 100, 20, hWnd, HMENU(0), g_hInstance, NULL);
+	_btnObjectDraw = CreateWindow("button", "Object",
+		// 자식으로 생성하면 안쪽에 만들어짐
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+		// 클릭했을때 어떤 값을 반환할지 hMenu
+		280, 80, 100, 20, hWnd, HMENU(1), g_hInstance, NULL);
+	_btnEraser = CreateWindow("button", "Eraser",
+		// 자식으로 생성하면 안쪽에 만들어짐
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+		// 클릭했을때 어떤 값을 반환할지 hMenu
+		280, 110, 100, 20, hWnd, HMENU(2), g_hInstance, NULL);
+
+	_btnSave = CreateWindow("button", "Save",
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+		280, 140, 100, 20, hWnd, HMENU(3), g_hInstance, NULL);
+
+	_btnLoad = CreateWindow("button", "Load",
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+		280, 170, 100, 20, hWnd, HMENU(4), g_hInstance, NULL);
+	isActive = false;
+}
+
+void Window::Release()
+{
+
+}
+
+void Window::Update()
+{
+	if (isActive &&
+		INPUT->GetKeyDown(VK_LBUTTON)) {
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 20; j++) {
+				if (PtInRect(&rc[i][j], ptMouse)) {
+					clickFrame = { i, j };
+				}
+			}
+		}
+	}
+
+	/////////////////////////////////////
+	int x, y, cx, cy;
+
+	RECT rcWin;
+	GetWindowRect(g_hWnd, &rcWin);
+
+	cx = SUBWINSIZEX;
+	cy = SUBWINSIZEY;
+	x = rcWin.right;
+	y = rcWin.top;
+
+	RECT rc;
+	rc.left = 0;
+	rc.top = 0;
+	rc.right = cx;
+	rc.bottom = cy;
+
+	AdjustWindowRect(&rc, WINSTYLE, FALSE);
+
+	SetWindowPos(hWnd, NULL, x, y,
+		(rc.right - rc.left), (rc.bottom - rc.top), SWP_NOZORDER);
+
+	if (currentScene != NULL)
+	{
+		currentScene->Update();
+	}
+}
+
+void Window::Render()
+{
+	HDC hdc = GetDC(hWnd);
+	PatBlt(m_backBuffer->GetMemDC(), 0, 0, SUBWINSIZEX, SUBWINSIZEY, WHITENESS);
+	//==============================
+	if (currentScene != NULL)
+	{
+		currentScene->Render(m_backBuffer->GetMemDC());
+	}
+	IMAGE->Render("tileMap", m_backBuffer->GetMemDC(), 0, 0);
+
+	//=============================
+	m_backBuffer->Render(hdc);
+	ReleaseDC(hWnd, hdc);
+}
+
+void Window::SetScene(GameNode * scene)
+{
+	currentScene = scene;
+	currentScene->Init();
+}
+
+LRESULT Window::WndLogProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg) {
+	case WM_MOUSEMOVE:
+		SUBWIN->SetIsActive(true);
+		ptMouse.x = LOWORD(lParam);
+		ptMouse.y = HIWORD(lParam);
+		break;
+	case WM_PAINT:
+	{
+		//HDC hdc = GetDC(hWnd);
+		//IMAGE->FindImage("sub_bg")->Render(hdc);
+		//ReleaseDC(hWnd, hdc);
+	}
+	break;
+	case WM_COMMAND:
+		// 프로그램 실행 중 사용자가 메뉴 항목을 선택하면 발생하는 메세지
+		switch (LOWORD(wParam))
+		{
+		default:
+			//// 클릭했을 때 0,1,2 값 중 하나가 들어옴 
+			//_currentCTRL = (CTRL)(LOWORD(wParam));
+			switch (LOWORD(wParam)) {
+			case CTRL_TERRAINDRAW:
+			case CTRL_OBJECTDRAW:
+			case CTRL_ERASER:
+				_currentCTRL = (CTRL)(LOWORD(wParam));
+				break;
+			case CTRL_SAVE:
+				SUBWIN->GetMainGame()->Save();
+				break;
+			case CTRL_LOAD:
+				SUBWIN->GetMainGame()->Load();
+				break;
+			}
+			break;
+		}
+		break;
+	case WM_KEYDOWN:
+		switch (wParam) {
+		case VK_ESCAPE:
+			PostQuitMessage(0);
+			break;
+		}
+		break;
+	default:
+		break;
+	}
+
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
