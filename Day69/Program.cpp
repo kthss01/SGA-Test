e8b836af23d2e1baba5a35b8ee883ec56f2963b5
kthss01 <kthss01@naver.com>
2018-04-25 13:13:34 +0900

    Day69 수업 준비

---
+++ Day69/Program.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "Program.h"
+
+
+Program::Program()
+{
+}
+
+
+Program::~Program()
+{
+}
+
+void Program::Update()
+{
+}
+
+void Program::Render()
+{
+}
---
+++ Day69/Program.h
@@ -0,0 +1,11 @@
+#pragma once
+class Program
+{
+public:
+	Program();
+	~Program();
+
+	void Update();
+	void Render();
+};
+
