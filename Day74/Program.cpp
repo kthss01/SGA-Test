5585beef5677182e55ee45556cef7f3e2c9b437b
kthss01 <kthss01@naver.com>
2018-05-03 12:57:04 +0900

    Day 74 수업 준비

---
+++ Day74/Program.cpp
@@ -0,0 +1,132 @@
+#include "stdafx.h"
+#include "Program.h"
+
+
+Program::Program()
+{
+	vertices = new Vertex[2];
+
+	vertices[0].position = Vector2(25, -55);
+	vertices[0].color = 0xFFFF0000;
+
+	vertices[1].position = Vector2(-50, 25);
+	vertices[1].color = 0xFFFF0000;
+
+	D2D::GetDevice()->SetRenderState(
+		// 라이트 지정
+		D3DRS_LIGHTING,
+		// 사용 여부
+		FALSE
+	);
+
+	vEye = Vector2(0, 0);	// 카메라의 위치
+	ray = Ray(Vector2(0, 0), Vector2(-1, 0));
+}
+
+Program::~Program()
+{
+	SAFE_DELETE_ARRAY(vertices);
+}
+
+void Program::Update()
+{
+	// 카메라 만드는거
+	Vector2 vLookAt(0, 0, 1);	// 바라보는 좌표값
+	vLookAt = vEye + Vector2(0, 0, 1);
+	Vector2 vUp(0, 1, 0);
+	matView = Matrix::View(vEye, vLookAt, vUp);
+	matProjection = Matrix::Ortho(0, WINSIZE_X, WINSIZE_Y, 0, 0.0, 1.0f);
+
+	if (GetKeyState(VK_UP) & 0x8000) { ray.direction.y += 0.1f; }
+	if (GetKeyState(VK_DOWN) & 0x8000) { ray.direction.y -= 0.1f; }
+	if (GetKeyState(VK_RIGHT) & 0x8000) { ray.direction.x += 0.1f; }
+	if (GetKeyState(VK_LEFT) & 0x8000) { ray.direction.x -= 0.1f; }
+
+	// 방향으로 지정해야되기 때문에 normalize 한번 해줘야함
+	ray.direction = ray.direction.Normalize();
+
+	Vector2 position[2];
+
+	Matrix matViewProj = matView * matProjection;
+
+	for (int i = 0; i < 2; i++) {
+		// world view projection 곱해줘야함 근데 월드는 identity라 안곱해도 됨
+		position[i] = vertices[i].position.TransformCoord(matViewProj);
+	}
+
+	// outPos 화면 좌표에서 2D좌표로 다시 바꿔주기위해 
+	// matVieProj에 역행렬을 곱해주는 거
+	if (Collision::IntersectRayToLine(
+		ray,
+		position[0],
+		position[1],
+		&outPos
+	)) {
+		float temp;
+		Matrix invMatrix = matViewProj.Inverse(temp);
+
+		outPos = outPos.TransformCoord(invMatrix);
+	}
+
+	ImGui::ColorEdit3("RayColor", color);
+}
+
+void Program::Render()
+{
+	// 실질적으로 계산하는건 Device로 넘겨주게됨
+	D2D::GetDevice()->SetTransform(D3DTS_VIEW, &matView.ToDXMatrix());
+	D2D::GetDevice()->SetTransform(D3DTS_PROJECTION, &matProjection.ToDXMatrix());
+
+	// FVF 정점에 데이터를 어떤 형식으로 쓸건지
+	// position, color 이걸 임의적으로 한거라 알려줘야함
+	// 구조체 들어간 순서와 동일하게 해줘야함
+	// XYZ 포지션 DIFFUSE color
+	// color값이랑 포지션 값을 꼭 넣어줘야함
+	// texture 띄우려면 texture coord를 넣어줘야함
+	// color 안넣어주면 실질적인 렌더는 안됨 좌표는 잇지만
+	//D2D::GetDevice()->SetFVF(D3DFVF_XYZ | D3DFVF_DIFFUSE);
+	// texture 로 사용할때
+	D2D::GetDevice()->SetFVF(D3DFVF_XYZ | D3DFVF_DIFFUSE);
+
+	D2D::GetDevice()->DrawPrimitiveUP(
+		D3DPT_LINELIST,		// 선임
+		// 점도 있음 D3DPT_POINTLIST
+		1, vertices, sizeof(Vertex)
+	);
+	/* lip 
+		vertex 들어간게 [0][1][2][3] -> [0][1], [1][2], [2][3]
+		ex) 삼각형 [0][1][2], [1][2][3] ...
+
+	   list (list list~~)
+		vertex 들어간게 [0][1][2][3] -> [0][1], [2][3]
+		ex) 삼각형 [0][1][2], [3][4][5] ...
+	*/
+
+	for (int i = 0; i < 10; i++) {
+		Matrix matWorld;
+		Vector2 move(i * 10, i * 10);
+		matWorld = Matrix::Translation(move);
+
+		D2D::GetDevice()->SetTransform(
+			D3DTS_WORLD, &matWorld.ToDXMatrix()
+		);
+		D2D::GetDevice()->DrawPrimitiveUP(
+			D3DPT_LINELIST,		// 선임
+								// 점도 있음 D3DPT_POINTLIST
+			1, vertices, sizeof(Vertex)
+		);
+	}
+
+	Vertex DrawRay[2];
+	DrawRay[0].position = ray.origin;
+	DrawRay[0].color = D3DXCOLOR(color);
+	
+	DrawRay[1].position = outPos;
+	DrawRay[1].color = D3DXCOLOR(color);
+
+	D2D::GetDevice()->DrawPrimitiveUP(
+		D3DPT_LINELIST,		// 선임
+							// 점도 있음 D3DPT_POINTLIST
+		1, DrawRay, sizeof(Vertex)
+	);
+}
---
+++ Day74/Program.h
@@ -0,0 +1,32 @@
+#pragma once
+
+// MainGame대신 돌아갈꺼
+
+class Program
+{
+private:
+	// 기본적으로 도형 만들려면 vertex 필요
+	struct Vertex {
+		Vector2 position;
+		DWORD color;
+	};
+
+	Vertex* vertices;
+	Matrix matView;
+	Matrix matProjection;
+	// viewPort 필요없음 
+	// 윈도우 쓰겠다고 선언된 순간부터 window 기준으로 되어있음
+	Vector2 vEye;
+	Vector2 outPos;
+
+	Ray ray;
+
+	float color[4];
+public:
+	Program();
+	~Program();
+
+	void Update();
+	void Render();
+};
+
